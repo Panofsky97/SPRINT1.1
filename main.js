443963057733ca2321cf1716e1d0c46a52a0724d
Panosky97 <mgallardopadilla7@gmail.com>
2025-05-26 11:28:53 +0200

    Add faq section

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+ function changeImages(fileName){
+   let img = document.querySelector("#bannerImage");
+   img.setAttribute("src", fileName)
+
+ }  
+  
   const menu      = document.querySelector('nav ul.nav-list');
-  const menuBtn = document.querySelector('.menu-open');
-  const closeBtn = document.querySelector('.menu-close');
+  const menuBtn   = document.querySelector('.menu-open');
+  const closeBtn  = document.querySelector('.menu-close');
 
   openBtn.addEventListener('click', () => {
     menu.classList.add('open');
